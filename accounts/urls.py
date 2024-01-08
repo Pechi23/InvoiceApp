e112de6d85a2e69d33758d78861bc2ab601f232e
George Pecherle <george.pecherle@gmail.com>
2024-01-08 12:35:12 +0000

    user login,logout and register managed

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('login/',views.login_view),
-    # path('logout/',views.logout_view),
-    # path('register/',views.register_view),
-    #fasfa
+    path('login/',views.login_view),
+    path('logout/',views.logout_view),
+    path('register/',views.register_view),
 ]
